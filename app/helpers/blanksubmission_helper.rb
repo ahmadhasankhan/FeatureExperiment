06a94c2d3cacaa7404272e39950724d9cf22fdbd
ahmadhasankhan <er.ahmad.hassan@gmail.com>
2015-08-03 00:16:04 +0530

    blank submission

---
+++ app/helpers/blanksubmission_helper.rb
@@ -0,0 +1,2 @@
+module BlanksubmissionHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'blanksubmission/index'
+  get 'blanksubmission/create'
+  get 'blanksubmission/show'
+
   resources :posts
 
   resources :messages
